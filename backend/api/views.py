f753d53ced38dcd258b3c777500c1b2578a6788b
Priscilla Baah <pyfbaah@gmail.com>
2023-01-27 23:54:28 +0000

    create drf api view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,23 +1,75 @@
-from django.http import JsonResponse
 import json
+# from django.http import JsonResponse, HttpResponse
+# from django.forms.models import model_to_dict
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+# using model data with django rest framework serializers
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    # print(dir(request))
-    print(request.GET)  # shows the url query params
+    """
+    DRF API view
+    """
+    instance = Product.objects.all().order_by('?').first()  # return random model object
+    data = {}
 
-    body = request.body  # byte string of JSON data
+    if instance:
+        data = ProductSerializer(instance).data
 
-    try:
-        data = json.loads(body)
-    except:
-        data = {}
+    return Response(data)
+
+
+
+# using model data with manual serialization
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by('?').first()  # return random model object
+
+#     data = {}
+#     if model_data:
+#         # manual serialization: model instance -> python dict -> json
+#         data['id'] = model_data.id
+#         data['title'] = model_data.title
+#         data['content'] = model_data.content
+#         data['price'] = model_data.price
+
+#         # using model_to_dict
+#         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+
+#         # return an HttpResponse instead
+#     #     json_data_string = json.dumps(data)
+
+#     # return HttpResponse(json_data_string, headers={'content-type':'application/json'})  
+#     # default content type for an HttpResponse is text/html
+#     # will need to serialize fields one by one
     
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    # headers are not json serializable by default so need to be converted to dict firs
-    data['content_type'] = request.content_type
+#     return JsonResponse(data)
+
+
+
+# using json data
+# def api_home(request, *args, **kwargs):
+#     # print(dir(request))
+#     print(request.GET)  # shows the url query params
+
+#     body = request.body  # byte string of JSON data
+
+#     try:
+#         data = json.loads(body)
+#     except:
+#         data = {}
+    
+#     data['params'] = dict(request.GET)
+#     data['headers'] = dict(request.headers)
+#     # headers are not json serializable by default so need to be converted to dict firs
+#     data['content_type'] = request.content_type
 
    
-    return JsonResponse(data)
+#     return JsonResponse(data)
+
+
