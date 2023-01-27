4dfe72160d6cb3d72fd41d33b3c1bc71d4572594
Priscilla Baah <pyfbaah@gmail.com>
2023-01-27 22:04:50 +0000

    create first api view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
 # Create your views here.
+
+def api_home(request, *args, **kwargs):
+    # print(dir(request))
+    body = request.body  # byte string of JSON data
+    print('body',body)
+
+    # data = json.loads(body)
+    # print('data', data)
+    
+    # try:
+    #     data = json.loads(body)
+    # except:
+    #     data = {}
+    #     print('empty data')
+    #     print(Exception)
+    return JsonResponse({"message":"Hi there, this is your Django API resonse!"})
