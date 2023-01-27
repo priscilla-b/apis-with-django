a77bac9074ca1217e62b6f56105185153d9a7cd7
Priscilla Baah <pyfbaah@gmail.com>
2023-01-27 22:05:55 +0000

    include params and json data in request object

--- py_client/basic_2.py
+++ py_client/basic_2.py
@@ -2,7 +2,13 @@
 
 import requests
 
-endpoint = "http://localhost:8000"
+endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)  # regular http request
-print(get_response.text)  # regular http response
+get_response = requests.get(
+    endpoint,
+    params={"abc":123},
+    json={"query":"hello world"}
+    )  # regular http request
+
+# print(get_response.url)
+print(get_response.json())  # regular http response
