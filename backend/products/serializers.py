daefb2fac4f67b34400ac0f0df4ac4cbf57eff25
Priscilla Baah <pyfbaah@gmail.com>
2023-01-31 03:24:56 +0000

    modify get discount method

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    my_discount = serializers.SerializerMethodField(read_only=True)
+    #   rename get_discount as my_discount in serializer
     class Meta:
         model = Product
         fields = [
@@ -10,5 +12,13 @@ class ProductSerializer(serializers.ModelSerializer):
             'content',
             'price',
             'sale_price',
-            'get_discount'
+            'my_discount'
         ]
+
+    def get_my_discount(self, obj):
+        # obj is an instance of the attached model
+        if not hasattr(obj,'id'):
+            return None
+        if not isinstance(obj, Product):
+            return None
+        return obj.get_discount()
