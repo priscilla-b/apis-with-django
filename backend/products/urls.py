25c52ce4865c02a75090d31cdbbbdfd7d268fb1c
Priscilla Baah <pyfbaah@gmail.com>
2023-01-31 05:21:07 +0000

    add update and destroy paths

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,8 +3,15 @@ from django.urls import path
 from . import views
 
 
-
+# /api/products/
 urlpatterns = [
-    path('', views.ProductCreateApiView.as_view()),
-    path('<int:pk>', views.ProductDetailApiView.as_view())
+    path('', views.ProductListCreateApiView.as_view()),
+    path('<int:pk>/', views.ProductDetailApiView.as_view()),
+    path('<int:pk>/update/', views.ProductUpdateApiView.as_view()),
+    path('<int:pk>/delete/', views.ProductUpdateApiView.as_view()),
 ]
+
+# urlpatterns = [
+#     path('', views.product_alt_view),
+#     path('<int:pk>', views.product_alt_view),
+# ]
