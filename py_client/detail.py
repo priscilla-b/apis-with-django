991b31dc298e854d8fd0654ad5902f624e2e2429
Priscilla Baah <pyfbaah@gmail.com>
2023-01-31 05:22:27 +0000

    get product detail response

--- py_client/detail.py
+++ py_client/detail.py
@@ -1,5 +1,5 @@
 import requests
 
-endpoint = "http://localhost:8000/api/products/200"
+endpoint = "http://localhost:8000/api/products/2/"
 get_response = requests.get(endpoint)
-# print(get_response.url)
+print(get_response.json())
