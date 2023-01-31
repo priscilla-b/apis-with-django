9c577386b07024f4fa3c891b51161ea81f76d9bc
Priscilla Baah <pyfbaah@gmail.com>
2023-01-31 04:05:05 +0000

    add product create view

--- backend/products/views.py
+++ backend/products/views.py
@@ -5,6 +5,20 @@ from .serializers import ProductSerializer
 
 
 
+class ProductCreateApiView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        print(serializer.validated_data)
+        title = serializer.validate_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+   
 class ProductDetailApiView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
