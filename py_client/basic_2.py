9f068e4a6f6e2ebba61b3bcccb20a7d5b937f89d
Priscilla Baah <pyfbaah@gmail.com>
2023-01-31 03:50:19 +0000

    get product request

--- py_client/basic_2.py
+++ py_client/basic_2.py
@@ -2,13 +2,15 @@
 
 import requests
 
-endpoint = "http://localhost:8000/api/"
+# endpoint = "http://localhost:8000/api/"
 
-get_response = requests.post(
-    endpoint,
-    params={"abc":123},
-    json={"content":"hello world"}
-    )  # regular http request
+# get_response = requests.post(
+#     endpoint,
+#     params={"abc":123},
+#     json={"content":"hello world"}
+#     )  # regular http request
 
+endpoint = "http://localhost:8000/api/products/200"
+get_response = requests.get(endpoint)
 # print(get_response.url)
 print(get_response.json())  # regular http response
