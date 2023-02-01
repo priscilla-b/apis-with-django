647fc9ecdafbcf65e1d68e96c82973ab7d52a2b4
Priscilla Baah <pyfbaah@gmail.com>
2023-02-01 03:44:49 +0000

    create an authentication request

--- py_client/list.py
+++ py_client/list.py
@@ -1,10 +1,23 @@
 import requests
+from getpass import getpass
 
 
+auth_endpoint = "http://localhost:8000/api/auth/"
+username = input('username: ')
+password = getpass()
 
-endpoint = "http://localhost:8000/api/products/"
+auth_response = requests.post(
+    auth_endpoint, 
+    json={'username':username, 'password':password}
+)
+print(auth_response.json())
 
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    endpoint = "http://localhost:8000/api/products/"
 
-# get_response = requests.get(endpoint)
-get_response = requests.get(endpoint)
-print(get_response.json())
+    get_response = requests.get(endpoint, headers=headers)
+    print(get_response.json())
