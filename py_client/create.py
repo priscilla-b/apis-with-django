21ba5026bdc73dfca7d004179da9462f6e1b48b6
Priscilla Baah <pyfbaah@gmail.com>
2023-02-06 06:37:41 +0000

    add authentication headers

--- py_client/create.py
+++ py_client/create.py
@@ -1,13 +1,14 @@
 import requests
 
 
+headers = {'Authorization': 'Bearer 2ce728093978ca34c3680be2f0cc0844a8ad6be3'}
 
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
-    'title': 'This field is done',
+    'title': 'Create with token',
     'price': 32.99
 }
 # get_response = requests.get(endpoint)
-get_response = requests.post(endpoint, json=data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 print(get_response.json())
