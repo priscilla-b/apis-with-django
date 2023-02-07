705f0d978c07a4d7ed26efca9e586504755cf739
Priscilla Baah <pyfbaah@gmail.com>
2023-02-07 00:59:42 +0000

    create staff permissions mixin

---
+++ backend/api/mixins.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+from .permissions import IsStaffEditorPermission
+
+
+class StaffEditorPermissionMixin():
+    permission_classes = [
+        permissions.IsAdminUser,
+        IsStaffEditorPermission
+    ]
