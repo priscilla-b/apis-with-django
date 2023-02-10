e6ff0e1b2f40e14db583487242be2206b3ceb599
Priscilla Baah <pyfbaah@gmail.com>
2023-02-10 07:52:50 +0000

    create mixin to filter queryset by user

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -8,3 +8,21 @@ class StaffEditorPermissionMixin():
         permissions.IsAdminUser,
         IsStaffEditorPermission
     ]
+
+
+class UserQuerySetMixin():
+    user_field = 'user'
+    allow_staff_view = False
+
+    def get_queryset(self, *args, **kwargs):
+        user = self.request.user
+
+        lookup_data = {}
+        lookup_data[self.user_field] = user
+        qs = super().get_queryset(*args, **kwargs)
+        
+        if user.is_staff and self.allow_staff_view:
+            return qs
+        return qs.filter(**lookup_data)
+        # using '**' unpacks the lookup data
+
