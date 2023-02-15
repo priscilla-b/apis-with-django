28f3b3b3d0e949d69e537152a2a995b197be6f9a
Priscilla Baah <pyfbaah@gmail.com>
2023-02-15 23:53:53 +0000

    add public field + model manager

--- backend/products/models.py
+++ backend/products/models.py
@@ -5,11 +5,16 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
+class ProductManager(models.Manager):
+    def search(self, query, user=None):
+        pass
+
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=1)
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price =  models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+    public = models.BooleanField(default=True)
 
     @property
     def sale_price(self):
