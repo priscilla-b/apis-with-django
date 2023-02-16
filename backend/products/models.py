e61fdae1c720f56f2df70cb2dd52ae4261475135
Priscilla Baah <pyfbaah@gmail.com>
2023-02-16 03:21:17 +0000

    add model queryset and manager classes

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,13 +1,35 @@
 from django.db import models
+from django.db.models import Q
 from django.conf import settings
 
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
 
-class ProductManager(models.Manager):
+class ProductQueryset(models.QuerySet):
+    def is_public(self):
+        return self.filter(public=True)
+
     def search(self, query, user=None):
-        pass
+
+        lookup = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = self.is_public().filter(lookup)
+
+        if user is not None:
+            qs2 = self.filter(user=user).filter(lookup)
+            # let user search within their own data even if it's not public
+            qs = (qs | qs2).distinct()
+
+        return qs
+
+class ProductManager(models.Manager):
+
+    def get_queryset(self):
+        return ProductQueryset(self.model, using=self._db)
+
+    def search(self, query, user=None):
+        # return Product.objects.filter(public=True).filter(title__icontains=query)
+        return self.get_queryset().search(query, user=user)
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=1)
@@ -15,6 +37,8 @@ class Product(models.Model):
     content = models.TextField(blank=True, null=True)
     price =  models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
     public = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     @property
     def sale_price(self):
