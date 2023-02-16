f6b41993d76289f4a6231c1c0394fa871eb19964
Priscilla Baah <pyfbaah@gmail.com>
2023-02-16 03:43:09 +0000

    include jwt authentication urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 from . import views
 
 urlpatterns = [
     path('auth/', obtain_auth_token),
-    path('', views.api_home)
+    path('', views.api_home),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
