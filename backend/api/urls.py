c4b7b7725108ff0ba37be8e21696d9d6c252cd9e
Priscilla Baah <pyfbaah@gmail.com>
2023-02-17 08:21:24 +0000

    add jwt token paths

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -12,7 +12,7 @@ from . import views
 urlpatterns = [
     path('auth/', obtain_auth_token),
     path('', views.api_home),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
